62e57730fcb592d0345b87bdbe68a1c338205e51
Kanen1323 <safari.rre.345@gmail.com>
2024-04-23 02:44:17 +0300

    add git ignore

---
+++ Apbd5/Dto/DtoAnimalFullInf.cs
@@ -0,0 +1,24 @@
+﻿namespace Apbd5.Dto;
+
+public class DtoAnimalFullInf
+{
+    public int _id { get; set; }
+    public string _name { get; set; }
+    public string _description { get; set; }
+    public string _category { get; set; }
+    public int _area { get; set; }
+
+    public DtoAnimalFullInf(int id, string name, string description, string category, int area)
+    {
+        this._id = id;
+        this._name = name;
+        this._description = description;
+        this._category = category;
+        this._area = area;
+    }
+
+    public override string ToString()
+    {
+        return $"id {_id} name {_name} description {_description} category {_category} area {_area}";
+    }
+}
---
+++ Apbd5/Dto/DtoAnimalNoID.cs
@@ -0,0 +1,25 @@
+﻿namespace Apbd5.Dto;
+
+public class DtoAnimalNoID
+{
+    public string _name { get; set; }
+    public string _description { get; set; }
+    public string _category { get; set; }
+    public int _area { get; set; }
+
+    public DtoAnimalNoID( string name, string description, string category, int area)
+    {
+        this._name = name;
+        this._description = description;
+        this._category = category;
+        this._area = area;
+    }
+
+    public override string ToString()
+    {
+        return $"name {_name} description {_description} category {_category} area {_area}";
+    }
+    public DtoAnimalNoID()
+    {
+    }
+}
